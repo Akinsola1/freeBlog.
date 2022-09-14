765fa49d134b14d6d28b49ff3c6bdf61fefc9d5e
Akinsola1 <akindoyinfaruq@gmail.com>
2022-09-14 08:19:48 +0100

    post fixes 👨‍🔧

---
+++ lib/secretKeys.dart
@@ -0,0 +1,7 @@
+class SecretKeys {
+  static const String apiKey = "AIzaSyC3oK59Y9jjSDtMQ7kNbI9m6DY2N34ZZOU";
+  static const String  appId = "1:179032654173:web:1fdfe76f590b36943fa8b4";
+  static const String  messagingSenderId = "179032654173";
+  static const String  projectId = "freeblog-e25b0";
+  static const String  storageBucket = 'freeblog-e25b0.appspot.com';
+}
